1f529fcbe7bcd16bd88ecec6bfd329f69754620b
doc-johnson <hustler@mail.ru>
2020-12-08 22:35:57 +0300

    DZ1

---
+++ src/Lesson_1/Fruit.java
@@ -0,0 +1,14 @@
+package Lesson_1;
+
+abstract class Fruit {
+
+    private float weight;
+
+    Fruit(float weight) {
+        this.weight = weight;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+}
